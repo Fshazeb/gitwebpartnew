eb29d4710c9f81b444ceb80d613ad420ec8c1475
Fshazeb <fshazeb@aitschool786.onmicrosoft.com>
2022-02-20 20:48:30 +0000

    first commit

---
+++ src/webparts/helloziyanWebpart/components/IHelloziyanWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface IHelloziyanWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/helloziyanWebpart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IHelloziyanWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'HelloziyanWebpartWebPartStrings' {
+  const strings: IHelloziyanWebpartWebPartStrings;
+  export = strings;
+}
