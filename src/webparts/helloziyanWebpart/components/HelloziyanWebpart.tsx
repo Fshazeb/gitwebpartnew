acfde4e712f6e290feab1b1993f72632b54b75c8
Fshazeb <fshazeb@aitschool786.onmicrosoft.com>
2022-02-20 22:02:46 +0000

    update 1.0

--- src/webparts/helloziyanWebpart/components/HelloziyanWebpart.tsx
+++ src/webparts/helloziyanWebpart/components/HelloziyanWebpart.tsx
@@ -22,7 +22,7 @@ export default class HelloziyanWebpart extends React.Component<IHelloziyanWebpar
           <div>Web part property value: <strong>{escape(description)}</strong></div>
         </div>
         <div>
-          <h3>Welcome to SharePoint Framework!</h3>
+          <h3>Faisal Welcome to SharePoint Framework!</h3>
           <p>
             The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
           </p>
